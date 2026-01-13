ce98ef23cc87b7472e3e1b5df3e893dc75c6f836
Wei Miao <wei.miao@kuleuven.be>
2026-01-13 17:54:38 +0100

    Add files via upload

--- reproduce package/siginificance test.R
+++ reproduce package/siginificance test.R
@@ -3,6 +3,8 @@
 # Import functions for simulation and empirical application
 #============================================================
 
+rm(list = ls())
+source('import functions for empirical application.R')
 
 #============================================================
 # Reproduce pair-wise difference test
@@ -80,7 +82,76 @@ MIDAS_LASSO = as.numeric(strsplit(data_test[3,1], ",")[[1]])[-1]
 n = length(MIDAS)
 p_value = 1-max(length(which(MIDAS > LASSO))/n)
 
+# s = 6 years, t = 8, 8.5, 9 years
 
+#sg-LASSO-M v.s. LASSOMIDAS
+
+#
+data_test = read.csv2("MIDAS_bootstrap6_AUC8.csv")
+dim(data_test)
+data_test[1,1]
+MIDAS <- as.numeric(strsplit(data_test[1,1], ",")[[1]])[-1]
+LASSO = as.numeric(strsplit(data_test[2,1], ",")[[1]])[-1]
+MIDAS_LASSO = as.numeric(strsplit(data_test[3,1], ",")[[1]])[-1]
+n = length(MIDAS)
+p_value = 1-max(length(which(MIDAS >= MIDAS_LASSO))/n)
+
+#
+data_test = read.csv2("MIDAS_bootstrap6_AUC8.5.csv")
+dim(data_test)
+data_test[1,1]
+MIDAS <- as.numeric(strsplit(data_test[1,1], ",")[[1]])[-1]
+LASSO = as.numeric(strsplit(data_test[2,1], ",")[[1]])[-1]
+MIDAS_LASSO = as.numeric(strsplit(data_test[3,1], ",")[[1]])[-1]
+n = length(MIDAS)
+p_value = 1-max(length(which(MIDAS >= MIDAS_LASSO))/n)
+
+
+#
+data_test = read.csv2("MIDAS_bootstrap6_AUC9.csv")
+dim(data_test)
+data_test[1,1]
+MIDAS <- as.numeric(strsplit(data_test[1,1], ",")[[1]])[-1]
+LASSO = as.numeric(strsplit(data_test[2,1], ",")[[1]])[-1]
+MIDAS_LASSO = as.numeric(strsplit(data_test[3,1], ",")[[1]])[-1]
+n = length(MIDAS)
+p_value = 1-max(length(which(MIDAS >= MIDAS_LASSO))/n)
+
+
+# s = 10 years, t = 13, 13.5, 14 years
+
+#sg-LASSO-M v.s. LASSOMIDAS
+#
+data_test = read.csv2("MIDAS_bootstrap10_AUC13.csv")
+dim(data_test)
+data_test[1,1]
+MIDAS <- as.numeric(strsplit(data_test[1,1], ",")[[1]])[-1]
+LASSO = as.numeric(strsplit(data_test[2,1], ",")[[1]])[-1]
+MIDAS_LASSO = as.numeric(strsplit(data_test[3,1], ",")[[1]])[-1]
+n = length(MIDAS)
+p_value = 1-max(length(which(MIDAS >= MIDAS_LASSO))/n)
+
+
+#
+data_test = read.csv2("MIDAS_bootstrap10_AUC13.5.csv")
+dim(data_test)
+data_test[1,1]
+MIDAS <- as.numeric(strsplit(data_test[1,1], ",")[[1]])[-1]
+LASSO = as.numeric(strsplit(data_test[2,1], ",")[[1]])[-1]
+MIDAS_LASSO = as.numeric(strsplit(data_test[3,1], ",")[[1]])[-1]
+n = length(MIDAS)
+p_value = 1-max(length(which(MIDAS >= MIDAS_LASSO))/n)
+
+
+#
+data_test = read.csv2("MIDAS_bootstrap10_AUC14.csv")
+dim(data_test)
+data_test[1,1]
+MIDAS <- as.numeric(strsplit(data_test[1,1], ",")[[1]])[-1]
+LASSO = as.numeric(strsplit(data_test[2,1], ",")[[1]])[-1]
+MIDAS_LASSO = as.numeric(strsplit(data_test[3,1], ",")[[1]])[-1]
+n = length(MIDAS)
+p_value = 1-max(length(which(MIDAS >= MIDAS_LASSO))/n)
 
 
 # s = 6 years, t = 8, 8.5, 9 years
