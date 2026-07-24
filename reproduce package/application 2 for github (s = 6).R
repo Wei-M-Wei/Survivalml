4a10ebb52d04bf046cee3ca187afdf302ade2b7d
Wei Miao <wei.miao@kuleuven.be>
2026-07-24 16:52:34 +0200

    Update application 2 for github (s = 6).R

--- reproduce package/application 2 for github (s = 6).R
+++ reproduce package/application 2 for github (s = 6).R
@@ -100,7 +100,7 @@ degree_macro = 2
 w_fin <- gb(degree = degree, alpha = 1/2, a = 0, b = 1, jmax = jmax)/jmax #lb(degree = degree, a = 0, b = 1, jmax = jmax)/jmax # gb(degree = degree, alpha = alpha, a = 0, b = 1, jmax = jmax)
 w_macro = gb(degree = degree_macro, alpha = 1/2, a = 0, b = 1, jmax = jmax)/jmax #lb(degree = degree, a = 0, b = 1, jmax = jmax)/jmax # gb(degree = degree, alpha = alpha, a = 0, b = 1, jmax = jmax)
 # tuning parameter grid search
-alpha = seq(0.9, 1, 0.01)
+alpha = c(0.8, 0.85, 0.9, 0.95, 1)
 
 #bootstrap size
 bootstrap_number = 1000
